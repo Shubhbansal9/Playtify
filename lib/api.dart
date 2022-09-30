c0e51b498b6ef60b33eac520bc86046aa367ecaf
Shubhbansal9 <shubhbansal44@gmail.com>
2022-10-01 00:15:08 +0530

    Initial commit

---
+++ lib/api.dart
@@ -0,0 +1,12 @@
+class SpotifyAPI {
+  //Initialise necessary variables from Spotify API
+  // var API_Token = '<Token>'
+  // Get List of music from playlist
+  void getMusic(String playlistURL) {
+
+  }
+  // Filter music according to genres
+  void filterMusic(String genre) {
+
+  }
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // Add Bottom Navigation Bar Here : Dashboard and Playlist Add/Remove View
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
